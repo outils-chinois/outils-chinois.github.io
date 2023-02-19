24abcccbe4f91b692097788f22927de8462c673d
weewoo <hello@how-are-u?>
2023-02-19 20:16:03 +0100

    Let's go!

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+const navbarHTML = `
+<a id="logo" title="Page d'accueil" href="index.html"><img alt="Webpage Logo" src="resources/images/logo_white.png"></a>
+<a href="flashcards/">Cartes Flash</a>
+<a href="#">Cours</a>
+`;
+
+function initializeNavbar() {
+	let navbarElement = document.createElement("nav");
+	navbarElement.innerHTML = navbarHTML;
+	document.body.appendChild(navbarElement);
+}
