0386842efe0f025c2c5d93e41908c372700a6e38
weewoo <hello@how-are-u?>
2023-04-30 10:55:18 +0200

    Entirely done with create page (except maybe documentation/help)

--- flashcards/create/create.js
+++ flashcards/create/create.js
@@ -23,14 +23,29 @@ const icon_list = [
     'fa-solid fa-plane'
 ];
 
-const current_char_list = [showTitleCard];
-
+const current_list = ['titleCardElement'];
+var current_index = 0
+var set_length = 0 // 1 less than actual length
 
 //-------------------------------Functions-------------------------------:
 
+next_card = () => goToIndex(current_index + 1);
+previous_card = () => goToIndex(current_index - 1);
+
 titleCardData = () => `${getCodeFromString(document.getElementById('setTitle').value)}|${getCodeFromString(icon_list.indexOf(document.getElementById('setIcon').className).toString())}`;
+mainCardRawData = () => ({
+    char: document.getElementById('mainDisplay').value,
+    pinyin: document.getElementById('pinyinInput').value,
+    definition: document.getElementById('definitionInput').value
+})
 
 randomizeIcon = () => document.getElementById('setIcon').className = icon_list[Math.round(Math.random() * (icon_list.length - 1) )];
+
+setCardData = (char="你好", pinyin="nǐ hǎo", definition="Bonjour") => {
+    document.getElementById('mainDisplay').value = char;
+    document.getElementById('pinyinInput').value = pinyin;
+    document.getElementById('definitionInput').value = definition;
+}
 
 enableCards = (...args) => {
     for (let attributeName of args) {
@@ -44,16 +59,90 @@ disableCards = (...args) => {
     }
 }
 
+updateIndex = () => {
+    let label = document.getElementById('indexLabel');
+    label.setAttribute('data-current_index', current_index);
+    label.setAttribute('data-set_length', set_length);
+
+    if (current_index === 0) {
+        enableCards('right_card');
+        disableCards('left_card');
+    }
+
+    else if (current_index == set_length) {
+        enableCards('left_card');
+        disableCards('right_card');
+    }
+    else {enableCards('left_card', 'right_card')}
+}
 
 function showCustomCard() {
     document.getElementById('mainCard').style.display = '';
     document.getElementById('titleCard').style.display = 'none';
-    enableCards('left_card');
+    document.getElementById('codeOutput').style.display = 'none'; // hide code output (save button)
+
+    document.getElementById('copyConfirmHint').setAttribute('data-show', false); // reset animation
     
 }
 
 function showTitleCard() {
     document.getElementById('mainCard').style.display = 'none';
     document.getElementById('titleCard').style.display = '';
-    disableCards('left_card');
+}
+
+function makeNewCard() {
+    if (current_index !== 0) {current_list[current_index] = mainCardRawData()} // save card to current slot (if not titleCard)
+
+    showCustomCard();
+    set_length++;
+    current_index = set_length; // go to new index
+    updateIndex();
+    setCardData(); // reset card data to default values
+}
+
+function goToIndex(index) {
+    // console.log(`current_index: ${current_index}; index: ${index}; char: ${current_list[index].char}`)
+    if (current_index !== 0) {current_list[current_index] = mainCardRawData()}; // save card
+
+    if (index !== 0) {
+        showCustomCard();
+        setCardData(current_list[index].char, current_list[index].pinyin, current_list[index].definition); // show card values
+    } else {
+        showTitleCard();
+    }
+
+    current_index = index; // set current index to chosen
+    updateIndex();
+}
+
+function saveDataToCode() {
+    var endCode = `${titleCardData()}|`; // save title card data -- 2 first elements are title card data
+    const charData = current_list.splice(1); // get every element from list safe from first
+    current_list.push(...charData); // restore splice
+
+    charData.forEach(element => {endCode += `${getCodeFromString(element.char)};${getCodeFromString(element.pinyin)};${getCodeFromString(element.definition)}|`})
+
+    return endCode.substring(0, endCode.length - 1)
+}
+
+function saveButton() {
+    if (current_index !== 0) {current_list[current_index] = mainCardRawData()}; // save card
+
+    let codeOutput = document.getElementById('codeOutput');
+    codeOutput.style.display = '';
+    codeOutput.innerHTML = saveDataToCode();
+
+    selectTextFromNode(codeOutput);
+    document.getElementById('copyConfirmHint').setAttribute('data-show', true);
+}
+
+function selectTextFromNode(htmlObject) {
+    currentRange = document.createRange();
+    currentRange.selectNodeContents(htmlObject);
+
+    selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(currentRange);
+
+    document.execCommand('copy'); // Copy to clipboard
 }
